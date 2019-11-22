522cc67a20e4d23a0fe89a1f0fbff786790455d1
amiriz <amir.izhaky@gmail.com>
2019-11-22 16:09:04 +0200

    fix some bugs in the calculation

--- PageRank.py
+++ PageRank.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-out_edges_dict = {}                     # adjacency list for all the out edges
-in_edges_dict = {}                      # adjacency list for all the in edges
-pages_rank_dict = {}                    # the page ranks of all the vertexes
+out_edges = {}                          # adjacency list for all the out edges
+in_edges = {}                           # adjacency list for all the in edges
+pages_ranks = {}                        # the page ranks of all the vertexes
 calculate_pagerank_activated = False    # flag which tells if we calculated the page ranks or not
 
 
@@ -20,60 +20,58 @@ def load_graph(path):
         dest = str(row[1][1])
 
         # initialize the out_edges and in_edges dicts:
-        if not out_edges_dict.get(source):
-            out_edges_dict[source] = [dest]
+        if not out_edges.get(source):
+            out_edges[source] = [dest]
         else:
-            out_edges_dict[source].append(dest)
+            out_edges[source].append(dest)
 
-        if not in_edges_dict.get(dest):
-            in_edges_dict[dest] = [source]
+        if not in_edges.get(dest):
+            in_edges[dest] = [source]
         else:
-            in_edges_dict[dest].append(source)
+            in_edges[dest].append(source)
 
         # initialize pages_rank_dict:
-        if not pages_rank_dict.get(source):
-            pages_rank_dict[source] = 0
-        if not pages_rank_dict.get(dest):
-            pages_rank_dict[dest] = 0
+        if not pages_ranks.get(source):
+            pages_ranks[source] = 0
+        if not pages_ranks.get(dest):
+            pages_ranks[dest] = 0
+
+        # fill additional empty nodes in out_edges and in in_edges:
+        for page in pages_ranks:
+            if page not in out_edges:
+                out_edges[page] = []
+            if page not in in_edges:
+                in_edges[page] = []
 
 
 # calculate the page ranks
 def calculate_page_rank(beta, delta, maxIterations):
-    next_pages_rank_dict = {}
-    sum_diff_smaller_then_delta = False
-    sum_rank = 0
-
     # iteration 0:
-    for page in pages_rank_dict:
-        pages_rank_dict[page] = 1/len(pages_rank_dict)
+    for page in pages_ranks:
+        pages_ranks[page] = 1 / len(pages_ranks)
+
+    cur_iteration = 0
+    diff = float('inf')
+    ranks_saver = {}
 
     # the iterations loop:
-    while maxIterations > 0 and not sum_diff_smaller_then_delta:
-        sum_diff = 0
+    while cur_iteration < maxIterations and diff > delta:
+        s = 0
+        diff = 0
         # the calculation of the temporary page ranks:
-        for page in pages_rank_dict:
-            sum_indeg = 0    # the new page rank value for the current page in the current iteration
-            if page not in in_edges_dict:
-                pages_rank_dict[page] = 0
-            if page in in_edges_dict:
-                for in_deg in in_edges_dict[page]:
-                    sum_indeg = sum_indeg + beta * (pages_rank_dict[in_deg] / len(out_edges_dict[in_deg]))
-                next_pages_rank_dict[page] = sum_indeg
-                sum_rank = sum_rank + sum_indeg
-        maxIterations = maxIterations - 1
+        for page in pages_ranks:
+            cur_rank = 0    # the new page rank value for the current page in the current iteration
+            for in_neighbor in in_edges[page]:
+                cur_rank += beta * (pages_ranks[in_neighbor] / len(out_edges[in_neighbor]))
+            s += cur_rank
+            ranks_saver[page] = cur_rank
+            diff += abs(cur_rank - pages_ranks[page])
 
-        # the calculation of the final page ranks:
-        for i in next_pages_rank_dict:
-            prev_pagerank = pages_rank_dict[i]
-            pages_rank_dict[i] = next_pages_rank_dict[i] + (1-sum_rank) / len(pages_rank_dict)
-            sum_diff = sum_diff + abs(pages_rank_dict[i]-prev_pagerank)
+        # the calculation of the final (not temporary) page ranks:
+        for page in pages_ranks:
+            pages_ranks[page] = ranks_saver[page] + ((1 - s) / len(pages_ranks))
 
-        # checking stop conditions:
-        if sum_diff < delta:
-            sum_diff_smaller_then_delta = True
-
-        next_pages_rank_dict = {}
-        sum_rank = 0
+        cur_iteration += 1
 
     global calculate_pagerank_activated
     calculate_pagerank_activated = True
@@ -83,23 +81,23 @@ def calculate_page_rank(beta, delta, maxIterations):
 def get_PageRank(node_name):
     if not calculate_pagerank_activated:
         return -1
-    if node_name not in pages_rank_dict:
+    if node_name not in pages_ranks:
         return -1
     else:
-        return pages_rank_dict[node_name]
+        return pages_ranks[node_name]
 
 
 # get number n and return the n nodes with the highest page rank ordered form the highest to the lowest
 def get_top_nodes(n):
     highest_pagerank_n_pages = list()
     sorted_pagerank_dict = get_all_PageRank()
-    if 0 < n < len(pages_rank_dict):
+    if 0 < n < len(pages_ranks):
         i = 0
         while i < n:
             highest_pagerank_n_pages.append(sorted_pagerank_dict[i])
             i = i + 1
         return highest_pagerank_n_pages
-    if n > len(pages_rank_dict):
+    if n > len(pages_ranks):
         return get_all_PageRank()
 
 
@@ -108,19 +106,19 @@ def get_all_PageRank():
     all_page_rank = list()
     if not calculate_pagerank_activated:
         return all_page_rank
-    sorted_pagerank_dict = sorted(pages_rank_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_pagerank_dict = sorted(pages_ranks.items(), key=lambda x: x[1], reverse=True)
     return sorted_pagerank_dict
 
 
 def main():
-    load_graph("C:\\Users\\Inbar\\Downloads\\facebook_combined.csv")
+    load_graph("C:\\Users\\amiri\\Desktop\\Wikipedia_votes.csv")
     # print("out_edges_dict")
     # print(out_edges_dict)
     # print("in_edges_dict")
     # print(in_edges_dict)
     # print("pages_rank_dict")
     calculate_page_rank(0.85, 0.001, 20)
-    print(pages_rank_dict)
+    print(get_top_nodes(10))
     """
     
     print("get_PageRank: 3")
